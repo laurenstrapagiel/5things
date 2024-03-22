355b33c801190793c0c562e5010eb93f9654d409
laurenstrapagiel <lauren.strapagiel@gmail.com>
2024-03-22 14:36:35 -0600

    adds basic header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,7 +4,17 @@ function Header() {
 
   return (
     <header className="header">
-      Welcome
+      <div className="header__left">
+        <h1 className="header__logo">5Things</h1>
+      </div>
+      <div className="header__right">
+        <ul className="header__nav">
+          <li className="header__nav-item">Exercise</li>
+          <li className="header__nav-item">Profile</li>
+          <li className="header__nav-item">About</li>
+          <li className="header__nav-item">Login</li>
+        </ul>
+      </div>
     </header>
   );
 }
