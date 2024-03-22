bdd56b4e63eb1c6785db81c06db5dcfaed2ae669
laurenstrapagiel <lauren.strapagiel@gmail.com>
2024-03-22 14:51:27 -0600

    builds basic homepage

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -3,9 +3,9 @@ import "./homepage.scss";
 function Homepage() {
   return (
     <section className="homepage">
-    <h1>Welcome!</h1>
-    <p>5Things is an anti-anxiety mindfulness exercise. Learn more about it here.</p>
-    <p>Login to track your exercises or click below to try it out.</p>
+    <h1 className="homepage__header">Welcome!</h1>
+    <p className="homepage__text">5Things is an anti-anxiety mindfulness exercise. Learn more about it here.</p>
+    <p className="homepage__text">Login to track your exercises or click below to try it out.</p>
     <div className="homepage__start-button">
       Get started
     </div>
