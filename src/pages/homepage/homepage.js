e6f0e98914d47da63e8d054f328feb72a7ab7ac4
laurenstrapagiel <lauren.strapagiel@gmail.com>
2024-03-22 14:41:16 -0600

    adds more styling

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -2,8 +2,9 @@ import "./homepage.scss";
 
 function Homepage() {
   return (
-    <section>
-    Hello
+    <section className="homepage">
+    <h1>Welcome!</h1>
+    <p>5Things is an anti-anxiety mindfulness exercise. Learn more about it here.</p>
     </section>
   );
 }
