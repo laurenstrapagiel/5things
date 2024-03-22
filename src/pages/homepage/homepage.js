e63b6258e745d9f8fda1dbee0f7ae75287661ba4
laurenstrapagiel <lauren.strapagiel@gmail.com>
2024-03-22 14:42:49 -0600

    starts homepage

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -5,6 +5,10 @@ function Homepage() {
     <section className="homepage">
     <h1>Welcome!</h1>
     <p>5Things is an anti-anxiety mindfulness exercise. Learn more about it here.</p>
+    <p>Login to track your exercises or click below to try it out.</p>
+    <div className="homepage__start-button">
+      Get started
+    </div>
     </section>
   );
 }
