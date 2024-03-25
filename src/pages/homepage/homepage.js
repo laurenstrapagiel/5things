143eefc7f8fbad1be8b49094050cb5b2eb6721c0
laurenstrapagiel <lauren.strapagiel@gmail.com>
2024-03-25 10:49:14 -0600

    improves homepage copy

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,14 +1,47 @@
 import "./homepage.scss";
+import OneIcon from "../../assets/images/1icon.svg";
+import TwoIcon from "../../assets/images/2icon.svg";
+import ThreeIcon from "../../assets/images/3icon.svg";
+import FourIcon from "../../assets/images/4icon.svg";
+import FiveIcon from "../../assets/images/5icon.svg";
+import { Link } from "react-router-dom";
+
 
 function Homepage() {
   return (
     <section className="homepage">
-    <h1 className="homepage__header">Welcome!</h1>
-    <p className="homepage__text">5Things is an anti-anxiety mindfulness exercise. Learn more about it here.</p>
-    <p className="homepage__text">Login to track your exercises or click below to try it out.</p>
-    <div className="homepage__start-button">
-      Get started
-    </div>
+      <h1 className="homepage__header">Welcome!</h1>
+      <h2 className="homepage__text">
+        5Things is an anti-anxiety mindfulness exercise that engages your senses
+        to ground yourself.
+      </h2>
+      <p>Do this exercise, you simply name:</p>
+      <ul>
+        <li className="homepage__list-item">
+          <img src={FiveIcon} className="homepage__icon"></img>
+          <h2 className="homepage__list-text"> Things you can see.</h2>
+        </li>
+        <li className="homepage__list-item">
+          <img src={FourIcon} className="homepage__icon"></img>
+          <h2 className="homepage__list-text"> Things you can touch.</h2>
+        </li>
+        <li className="homepage__list-item">
+          <img src={ThreeIcon} className="homepage__icon"></img>
+          <h2 className="homepage__list-text"> Things you can hear.</h2>
+        </li>
+        <li className="homepage__list-item">
+          <img src={TwoIcon} className="homepage__icon"></img>
+          <h2 className="homepage__list-text"> Thing you can smell.</h2>
+        </li>
+        <li className="homepage__list-item">
+          <img src={OneIcon} className="homepage__icon"></img>
+          <h2 className="homepage__list-text"> Thing you can taste.</h2>
+        </li>
+      </ul>
+      <p>
+        Read about why this an effective exercise <Link to="/about">here</Link>{" "}
+        or <Link to="/login">login</Link> to get started.
+      </p>
     </section>
   );
 }
